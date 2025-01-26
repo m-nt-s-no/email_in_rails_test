cb7143dfcd3342db98b4d8be555798af1da60942
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2025-01-26 21:13:59 +0000

    added letter_opener to gemfile and development envt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Allow server to be hosted on any URL
   config.hosts.clear
   # Allow better_errors to work in online IDE
